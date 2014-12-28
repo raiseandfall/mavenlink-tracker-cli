2bd6e17c4956fbc2c15218a9a6869ce3edf696cd
raiseandfall <matthew@jvst.us>
2014-12-27 19:07:28 -0600

    [FEATURE] Removed prompt; uses Inquirer.js for better choice UI

--- index.js
+++ index.js
@@ -5,15 +5,13 @@
 var events  = require('events'),
   evt       = new events.EventEmitter(),
   mavenApi  = require('./lib/mavenapi'),
-  Tracker   = require('./lib/tracker'),
-  pr        = require('prompt');
+  Tracker   = require('./lib/tracker');
 
 // Start prompt
 pr.start();
 
 // When authenticated
 evt.on('authenticated', function (token) {
-  // Test API Call
   var track = new Tracker(token);
   track.track(function(data) {
     console.log('You added '+data.hours+'h on '+data.date_performed+' for ' + data.story + ' on '+data.workspace);
